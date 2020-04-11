e7ccd68558609415c53bd1a4c165a73ae12541a4
Hayden Pennington <haydenpennington@icloud.com>
2020-04-11 00:06:43 -0500

    Save progress

--- www/api/routes/api.php
+++ www/api/routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/query', 'QueryController@query');
+Route::get('/databases', 'QueryController@databases');
+Route::get('/tables', 'QueryController@tables');
