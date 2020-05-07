5b9a9088299324c812d09ebf9b04842b8305ad3b
Hayden Pennington <haydenpennington@icloud.com>
2020-05-06 22:42:37 -0500

    Add bad pagination

--- dbgui/src/features/spreadsheet/spreadsheet-container.js
+++ dbgui/src/features/spreadsheet/spreadsheet-container.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Pagination } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { setColumns, setRows } from './spreadsheetSlice'
 import Spreadsheet from './spreadsheet'
@@ -8,21 +9,14 @@ function SpreadsheetContainer(props) {
 
   function onScrollUp() {
     console.log('onScrollUp')
-    const localOffset = offset - 50
-    setOffset(localOffset)
-  }
-
-  function onScrollDown() {
-    console.log('onScrollDown')
-
-    const localOffset = offset + 50
+    const localOffset = offset - 100
     const database = 'pms'
     const table = 'key_sku'
 
     fetch('http://localhost:3000/table?database='
       + database
       + '&table=' + table
-      + '&offset=' + offset,
+      + '&offset=' + localOffset,
       {
       method: 'POST',
       headers: {
@@ -33,7 +27,41 @@ function SpreadsheetContainer(props) {
       .then(res => {
         if (res.columns !== undefined && res.rows !== undefined) {
           props.dispatch(setColumns({columns: res.columns}))
-          const newRows = props.rows.concat(res.rows.map(row => Object.values(row)))
+          const newRows = res.rows.map(row => Object.values(row))
+          console.log(newRows)
+          props.dispatch(
+            setRows({rows: newRows})
+          )
+        }
+
+        setOffset(localOffset)
+      })
+      .catch(err => window.alert(err))
+
+  }
+
+  function onScrollDown() {
+    console.log('onScrollDown')
+
+    const localOffset = offset + 100
+    const database = 'pms'
+    const table = 'key_sku'
+
+    fetch('http://localhost:3000/table?database='
+      + database
+      + '&table=' + table
+      + '&offset=' + localOffset,
+      {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({'api_key': process.env.REACT_APP_API_KEY}),
+    }).then(res => res.json())
+      .then(res => {
+        if (res.columns !== undefined && res.rows !== undefined) {
+          props.dispatch(setColumns({columns: res.columns}))
+          const newRows = res.rows.map(row => Object.values(row))
           console.log(newRows)
           props.dispatch(
             setRows({rows: newRows})
@@ -45,11 +73,25 @@ function SpreadsheetContainer(props) {
       .catch(err => window.alert(err))
   }
 
-  return (<Spreadsheet
-    offset={offset}
-    scrollUp={onScrollUp}
-    scrollDown={onScrollDown}
-    columns={props.columns} rows={props.rows} />)
+  return (
+    <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}>
+      <Spreadsheet
+        offset={offset}
+        scrollUp={onScrollUp}
+        scrollDown={onScrollDown}
+        columns={props.columns} rows={props.rows} />
+        <div style={{
+            margin: 'auto'
+          }}>
+          <Pagination>
+            <Pagination.Prev onClick={onScrollUp} />
+            <Pagination.Next onClick={onScrollDown} />
+          </Pagination>
+        </div>
+      </div>)
 }
 
 const mapStateToProps = state => {
--- dbgui/src/features/spreadsheet/spreadsheet.js
+++ dbgui/src/features/spreadsheet/spreadsheet.js
@@ -2,39 +2,6 @@ import React from 'react'
 import './spreadsheet.css'
 
 export default class Spreadsheet extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      previousY: 0.0,
-      previousRows: 0,
-      mounted: false,
-    }
-  }
-
-  componentDidUpdate() {
-    if (this.refs.table.children.length >= 50 && this.props.rows.length !== this.state.previousRows) {
-      this.setState({previousRows: this.props.rows.length})
-    }
-  }
-
-  setupIntersectionObserver() {
-      const intersectionObserver = new IntersectionObserver(function(entries) {
-        for (const entry of entries) {
-          if (entry.boundingClientRect.y !== this.state.previousY) {
-            if (entry.boundingClientRect.y > 0) {
-              if (entry.intersectionRect.y <= 0) {
-                this.props.scrollDown()
-                this.setupIntersectionObserver()
-              }
-            }
-          }
-        }
-      }.bind(this))
-
-      intersectionObserver.observe(this.refs.table.children[this.props.offset + 50])
-  }
-
   render() {
     return (
       <div className="spreadsheet">
